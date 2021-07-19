ed3dc838599ff23200a551be4b10eea2ac8bc5f6
Daniel Pilař <danekpilar01@gmail.com>
2021-07-19 11:25:50 +0200

    Add image generating and saving

--- src/CAGeneration.java
+++ src/CAGeneration.java
@@ -1,5 +1,9 @@
 import processing.core.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CAGeneration extends PApplet{
 
     CellGrid cellGrid;
@@ -8,15 +12,17 @@ public class CAGeneration extends PApplet{
         PApplet.main("CAGeneration");
     }
 
+    private final int resolution = 300;
+
+    private ArrayList<Integer> rulesSequence;
+
     @Override
     public void settings() {
         size(900,900);
     }
 
-    @Override
-    public void setup() {
-
-        final int resolution = 150;
+    private void createImage(int rule){
+        Cell.setRules(rule);
 
         cellGrid = new CellGrid(resolution,this.width/resolution);
 
@@ -24,6 +30,25 @@ public class CAGeneration extends PApplet{
             cellGrid.show(this);
             cellGrid.nextGeneration();
         }
+    }
+
+    @Override
+    public void setup() {
+        int rulesNum = 256;
+        rulesSequence = new ArrayList<>();
+        for(int i = 0; i < rulesNum; i ++){
+            rulesSequence.add(i);
+        }
+
+        Collections.shuffle(rulesSequence);
+
+        for(int i = 0; i < rulesNum; i++){
+            clear();
+            int currentRule = rulesSequence.get(i);
+            createImage(currentRule);
+            save("out/rule_"+Integer.toString(currentRule)+".png");
+        }
+        exit();
     }
 
     @Override
--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,28 @@
+class Tools{
+    public static int[] createRule(int num){
+        if(num >= 0 && num < 256){
+            int[] output = new int[8];
+            String shortBinary = Integer.toBinaryString(num);
+            char[] array = ("00000000"+shortBinary).substring(shortBinary.length()).toCharArray();
+            for(int i = 0; i < array.length;i++){
+                output[i]=Integer.parseInt(Character.toString(array[i]));
+            }
+            return output;
+        }
+        return new int[]{0,0,0,0,0,0,0,0};
+    }
+}
+
 public class Cell {
 
     private static final int[] possibleStates = {0,1};
 
     private int state;
-    private static final int[] rules = {0,1,0,1,1,0,1,0};
+    static int[] rules = {0,0,0,0,0,0,0,0};
+
+    public static void setRules(int rule){
+        Cell.rules = Tools.createRule(rule);
+    }
 
     public Cell(int state){
         this.state = state;
