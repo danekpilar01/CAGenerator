bf8d82835407bea908e7f9cfaa418fa542f2784c
Daniel Pilař <danekpilar01@gmail.com>
2022-06-08 17:00:45 +0200

    Change default init values

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,12 +1,10 @@
-
-
 public class MainClass {
 
 
     public static void main(String[] args){
 
         try {
-            new CAGenerate(600, 600, 60,55,20).run();
+            new CAGenerate(600, 600, 60,55,120).run();
         }catch (IllegalArgumentException e){
             System.out.println("Cannot generate images with given ruleset.");
         }
